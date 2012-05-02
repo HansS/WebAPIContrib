e917c9ba073a91340f42747d20c1bd792befae89
Darrel <darrel@tavis.ca>
2012-05-02 14:22:28 -0400

    Added FileContent and SimpleFormatter

---
+++ src/WebApiContrib/Content/FileContent.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiContrib.Content
+{
+    public class FileContent : HttpContent
+    {
+        private readonly FileStream _Content;
+
+        public FileContent(string filename)
+        {
+            _Content = new FileStream(filename, FileMode.Open);
+        }
+
+
+        protected override Task<Stream> CreateContentReadStreamAsync()
+        {
+            return Task.Factory.StartNew<Stream>(() => _Content);
+        }
+
+        protected override Task SerializeToStreamAsync(Stream stream, TransportContext context)
+        {
+            return Task.Factory.StartNew(() => _Content.CopyTo(stream));
+        }
+
+
+
+        protected override bool TryComputeLength(out long length)
+        {
+            length = _Content.Length;
+            return true;
+        }
+    }
+}
---
+++ src/WebApiContrib/Formatting/SimpleFormatter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiContrib.Formatting
+{
+    public class SimpleFormatter<T> : MediaTypeFormatter
+    {
+        private readonly Action<T, Stream> _write;
+        private readonly Func<Stream, T> _read;
+
+        public SimpleFormatter(MediaTypeHeaderValue mediatype, Func<Stream, T> read, Action<T, Stream> write)
+        {
+            _write = write;
+            _read = read;
+            SupportedMediaTypes.Add(mediatype);
+        }
+
+        protected override bool CanReadType(Type type)
+        {
+            return _read != null && type == typeof(T);
+        }
+
+        protected override bool CanWriteType(Type type)
+        {
+            return _write != null && type == typeof(T);
+        }
+
+        protected override System.Threading.Tasks.Task<object> OnReadFromStreamAsync(Type type, Stream stream, HttpContentHeaders contentHeaders, FormatterContext formatterContext)
+        {
+            return new TaskFactory<object>().StartNew(() => _read(stream));
+        }
+
+
+        protected override Task OnWriteToStreamAsync(Type type, object value, Stream stream, HttpContentHeaders contentHeaders, FormatterContext formatterContext, TransportContext transportContext)
+        {
+            return new TaskFactory().StartNew(() => _write((T)value, stream));
+        }
+
+
+    }
+}
